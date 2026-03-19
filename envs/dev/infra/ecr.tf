bc5c9d799d9b62ead13ddfd6fc8703ca4457f61e
kiyun-kim <kiyn2403@gmail.com>
2026-03-19 18:42:39 +0900

    fix(ecr): lifecycle rule 수정 -> 7일 지난 태그 없는 이미지 삭제 규칙 제거

--- envs/dev/infra/ecr.tf
+++ envs/dev/infra/ecr.tf
@@ -10,29 +10,16 @@ module "ecr_backend" {
   scan_on_push         = true
   force_delete         = true
 
-  # 이미지 10개만 유지 + 7일 지난 태그 없는 이미지 삭제
+  # 이미지 10개만 유지
   lifecycle_policy_json = jsonencode({
     rules = [
-      {
-        "rulePriority" : 2,
-        "description" : "Delete untagged images after 7 days",
-        "selection" : {
-          "tagStatus" : "untagged",
-          "countType" : "sinceImagePushed",
-          "countUnit" : "days",
-          "countNumber" : 7
-        },
-        "action" : {
-          "type" : "expire"
-        }
-      },
       {
         "rulePriority" : 1,
         "description" : "Keep last 10 images",
         "selection" : {
           "tagStatus" : "any",
           "countType" : "imageCountMoreThan",
-          "countNumber" : 5
+          "countNumber" : 10
         },
         "action" : {
           "type" : "expire"
@@ -64,25 +51,12 @@ module "ecr_frontend" {
   lifecycle_policy_json = jsonencode({
     rules = [
       {
-        "rulePriority" : 2,
-        "description" : "Delete untagged images after 7 days",
-        "selection" : {
-          "tagStatus" : "untagged",
-          "countType" : "sinceImagePushed",
-          "countUnit" : "days",
-          "countNumber" : 7
-        },
-        "action" : {
-          "type" : "expire"
-        }
-      },
-      {
         "rulePriority" : 1,
         "description" : "Keep last 10 images",
         "selection" : {
           "tagStatus" : "any",
           "countType" : "imageCountMoreThan",
-          "countNumber" : 5
+          "countNumber" : 10
         },
         "action" : {
           "type" : "expire"
